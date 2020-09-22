f4ff5dba8b90a6270cf9582f8399cb39e447740c
Chris Wood <cjw@stowers.org>
2020-09-22 09:41:01 -0500

    removed commeted code

--- simple_setup.py
+++ simple_setup.py
@@ -8,15 +8,3 @@ setup(
     ext_modules=cythonize("simple/sumpy_cy.pyx"),
     zip_safe=False,
 )
-# setup(
-#     name='sumpyx',
-#     ext_modules=cythonize(
-#         Extension(
-#             "simple.sumpy_cy",
-#             sources=["simple/sumpy_cy.pyx"],
-#             include_dirs=[np.get_include()]
-#         )
-#     ),
-#     install_requires=["numpy"],
-#     zip_safe=False,
-# )
