0e9d9cde65f3af28eee2a4bcd0128399354a4287
Chris Wood <cjw@stowers.org>
2020-10-08 10:45:41 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
+import setuptools
 from Cython.Build import cythonize
 
 setup(
     name="Find Maxima",
     install_requires=['numpy'],
     ext_modules=cythonize("findmaxima/cy_findmaxima.pyx"),
+    packages=setuptools.find_packages(),
     zip_safe=False,
 )
