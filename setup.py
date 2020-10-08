2f3b75580cd7a9839e86a027743655bdd11ae856
Chris Wood <cjw@stowers.org>
2020-10-08 10:02:28 -0500

    added required to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     name="Find Maxima",
+    install_requires=['numpy', 'cython'>=0.29],
     ext_modules=cythonize("findmaxima/cy_findmaxima.pyx"),
     zip_safe=False,
 )
