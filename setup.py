fc4d64681ee9dffb8cedf8cd330495c69f8c5b04
Chris Wood <cjw@stowers.org>
2021-02-24 10:34:24 -0600

    added readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 extensions = Extension("cy_findmaxima", ["findmaxima/cy_findmaxima.pyx"])
 setup(
     name="pyfindmaxima",
-    version="0.1.0.3",
+    version="0.1.0.4",
     setup_requires=["cython", "numpy"],
     install_requires=['numpy'],
     ext_modules=cythonize(["findmaxima/cy_findmaxima.pyx"]),
