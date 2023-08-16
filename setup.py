c89b94005345b86cfa3ee65e4f3e3e8d6ac884f7
Chris Wood <cjw@stowers.org>
2023-08-16 14:15:51 -0500

    version update to 0.1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 extensions = Extension("cy_findmaxima", ["findmaxima/cy_findmaxima.pyx"])
 setup(
     name="pyfindmaxima",
-    version="0.1.1.0",
+    version="0.1.1.1",
     setup_requires=["cython", "numpy"],
     install_requires=['numpy'],
     ext_modules=cythonize(["findmaxima/cy_findmaxima.pyx"]),
