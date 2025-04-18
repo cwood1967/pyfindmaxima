f8557a225ffe71f81e95a4d6f484a3b044e12b60
Chris Wood <cjw@stowers.org>
2025-04-18 13:35:29 -0500

    added files for pixi

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
+import os
 from setuptools import Extension, setup
-import setuptools
 from Cython.Build import cythonize
+import numpy
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 extensions = Extension("cy_findmaxima", ["findmaxima/cy_findmaxima.pyx"])
 setup(
-    name="pyfindmaxima",
-    version="0.1.0.2",
-    setup_requires=["cython", "numpy"],
-    install_requires=['numpy'],
     ext_modules=cythonize(["findmaxima/cy_findmaxima.pyx"]),
-    #ext_modules=cythonize(extensions),
-    package_data = {'findmaxima':['*.pyx']},
-    packages=setuptools.find_packages(),
-    zip_safe=False,
 )
+
